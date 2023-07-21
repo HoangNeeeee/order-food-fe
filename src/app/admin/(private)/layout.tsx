1d405d1c7b264a40cab177515206ba8ad0326525
Nguyen Hoang <hoangn@vnpt.vn>
2023-07-21 08:32:01 +0700

    update (wip): admin authen

--- src/app/admin/(private)/layout.tsx
+++ src/app/admin/(private)/layout.tsx
@@ -5,10 +5,7 @@ import { redirect } from "next/navigation";
 export default async function AdminLayout({ children }: { children: React.ReactNode }) {
 	const session = await getServerSession(authOptions);
 
-	console.log("session", session);
-
 	if (!session) {
-		// redirect("/admin/login");
 		redirect("/api/auth/signin");
 	}
 	return <div>{children}</div>;
