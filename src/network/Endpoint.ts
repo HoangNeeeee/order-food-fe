448eb7cbfc7a0bb9b5882abee6e42b9cb5d85a10
HoangNeeeee <hn24101991@gmail.com>
2023-08-08 15:02:42 +0700

    update: api endpoint

--- src/network/Endpoint.ts
+++ src/network/Endpoint.ts
@@ -1,7 +1,9 @@
 const Endpoint = {
 	LOGIN: "/api2/auth/login",
-	SIDEBAR_MENU: "/v1/menu",
-	CREATE_SIDEBAR_MENU: "/v1/menu/create",
+	SIDEBAR_MENU: "/v1/menu/sidebar",
+	CREATE_SIDEBAR_MENU: "/v1/menu/sidebar/create",
+	CREATE_SIDEBAR_EDIT: "/v1/menu/sidebar/edit",
+	CREATE_SIDEBAR_DELETE: "/v1/menu/sidebar/delete",
 };
 
 export default Endpoint;
